f5e29af7b09eaaa544918696bf95fecb66d02ac3
Jeremy Hopkins <hopkinsje09@students.ecu.edu>
2020-11-17 23:30:21 -0500

    added common html elements

--- edit_task.php
+++ edit_task.php
@@ -1,12 +1,89 @@
 <?php
-    require_once("database.php");
+	require_once('valid_user.php'); 
+	require_once("database.php");
     require_once("user_db.php");
     require_once("task_db.php");
     
     // Get the task data from the form
     $tid = filter_input(INPUT_POST, "task_id");
-    $task = get_task($tid);
+	$task = get_task($tid);
+	$max_volunteers = 10;	//maximum number of volunteers needed for a task
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Final project</title>   
+</head>
+
+
+
+<body>
+	<header>
+		<h1>East Carolina University </h1>
+		<h1>Cultural Center</h1>
+		<img src=images/worldpeacelogo.jpeg alt="worldpeacelogo" width="150">	
+	</header>
+	<style>
+		header {
+		background-image: -webkit-linear-gradient(45deg, white 0%, green 75%, black 110%);
+		background-image: -moz-linear-gradient(45deg, white 0%, green 75%, black 110%);
+		background-image: -o-linear-gradient(45deg, white 0%, green 75%, black 110%%);
+		background-image: linear-gradient(45deg, white 0%, green 75%, black 110%);
+		border: 2px solid black;
+		text-align: center;
+		}
+		header img { 
+		float:left;
+		margin-top:0em;
+		margin-bottom:20em;
+		} 
+		
+		fieldset {
+		margin: .5em;
+		border:1px solid black;
+		Font-size: 150%;
+		}
+		legend {
+		font-weight: bold;
+		font-size: 120%;
+		text-align: left;
+		}
+		label {
+		color: blue;
+		width: 6em;
+		padding-right: 1em;
+		position: absolute;
+		}
+		th  {
+		color: blue;
+		width: 6em;
+		padding-right: 1em;
+		text-align: left;
+		font-weight: normal;
+		}				
+		select {
+		width: 10em;
+		margin-bottom: 1em;
+		margin-left:12em;
+		font-weight: bold;
+		}			
+		input {   
+		margin-left: 12em;
+		margin-bottom: 1em;
+		font-weight: bold;
+		width: 12em;			   
+		}		
+		#button {
+		width: 12em; 
+		background-color: rgb(192, 192, 192);
+		border: 1px solid black;  
+		margin-left: 12em;
+		}
+		p {
+		color: blue;
+		}
+	</style>
 
 <fieldset>
 		<legend>Edit Task</legend>
@@ -43,4 +120,7 @@
 			<input type="submit" id="button" value="Update Task">
 		</form>
 	</fieldset>
+</body>
+</html>
+
 
