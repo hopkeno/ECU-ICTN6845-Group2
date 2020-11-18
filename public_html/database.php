1160c2b7686c8688ea71359d3983b4b2effebab4
Jeremy Hopkins <hopkinsje09@students.ecu.edu>
2020-11-17 22:58:29 -0500

    database params are in the env now

--- public_html/database.php
+++ public_html/database.php
@@ -1,10 +1,15 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=taskdb';
-    $username = 'taskdb_admin';
-    $password = 'password';
+    $dsn = $_ENV['DB_DSN'];
+    $username = $_ENV['DB_USER'];
+    $password = $_ENV['DB_PASS'];
+    $options = array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        PDO::MYSQL_ATTR_SSL_CA => '/path/to/cacert.pem',
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+    );
     global $db;
     try {
-        $db = new PDO($dsn, $username, $password); //creates PDO object
+        $db = new PDO($dsn, $username, $password, $options); //creates PDO object
     } catch (PDOException $e) {
         $err_msg = $e->getMessage();
         include('database_error.php');
