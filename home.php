95015e1b0a0213a6faece7acab2b6b4535686e21
Jeremy Hopkins <hopkinsje09@students.ecu.edu>
2020-11-18 12:12:33 -0500

    put correct form and actions back in place.  were removed in commit bdebfe9d724ed4ec5b0497eced6b4baebdbb60f1

--- home.php
+++ home.php
@@ -25,41 +25,28 @@
 	
 	<h2>Volunteers page</h2>
     <main>
-    <p>In order to sign up for tasks, you need to register first.</p>
-	<p>Already registered, please click <a href="success.php">here and login</a> to your account.</p>
+    <p>In order to sign up for tasks, you need to register first.</p> 
+	<p>Already registered, please click <a href="login.php">here and login</a> to your account.</p>
    
-    <form action="success.php" method="post" >
-	
-    <fieldset>
-        <legend>Registration</legend>
-		
-       
-        <label>First Name:</label>
-		<input type="text" name="first_name" value=""><br><br>
-       
-        <label>Last Name:</label>
-        <input type="text" name="last_name" value=""><br><br>
-              
-      
-        <label>E-Mail:</label>
-        <input type="text" name="email" value=""><br><br>
-			   
-		<label>Create Username:</label>
-		<input type="text" name="username" value=""><br><br>
-			   
-			   
-        <label>Create Password:</label>
-		<input type="text" name="password"  value=""><br><br>	
-			   
-		<label>Verify Password:</label>
-		<input type="text" name="password"  value=""><br><br>	  
-		   
-		
-        <input type="submit"  id="button" value="Register"><br><br>
-		
-    </fieldset>
-  
-          
+   <form action="add_user.php" method="post" id="login_form">
+	   <fieldset>
+		   <legend>Registration</legend>
+		   <label>First Name:</label>
+		   <input type="text" name="first_name" value=""><br><br>
+		   <label>Last Name:</label>
+		   <input type="text" name="last_name" value=""><br><br>
+		   <label>E-Mail:</label>
+		   <input type="text" name="email" value=""><br><br>
+		   <label>Create Username:</label>
+		   <input type="text" name="username" value=""><br><br>
+		   <label>Create Password:</label>
+		   <input type="password" name="password"  value=""><br><br>	
+		   <label>Verify Password:</label>
+		   <input type="password" name="password2"  value=""><br><br>	  
+		   <input type="submit"  id="button" value="Register"><br><br>
+	   </fieldset>
+   </form>
+
 		  
 
 	<style>
