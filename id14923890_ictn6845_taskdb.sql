7332e599d08012722acd75f62989104a5f27eeb9
Jeremy Hopkins <hopkinsje09@students.ecu.edu>
2020-11-17 22:00:34 -0500

    updated db configs

--- id14923890_ictn6845_taskdb.sql
+++ id14923890_ictn6845_taskdb.sql
@@ -2,6 +2,8 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+CREATE DATABASE IF NOT EXISTS id14923890_ictn6845_taskdb;
+use id14923890_ictn6845_taskdb;
 -- --------------------------------------------------------
 --
 -- Database: `id14923890_ictn6845_taskdb`                      
@@ -14,6 +16,9 @@ CREATE TABLE `users` (
   `volunteerID` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(1024) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255),
+  `email` varchar(255),
+  `first_name` varchar(50),
+  `last_name` varchar(50),
   `is_admin` boolean NOT NULL DEFAULT FALSE,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -27,7 +32,7 @@ CREATE TABLE `tasks` (
   `volunteerID` int(11),
   `assignerID` int(11),
   `title` varchar(512) COLLATE utf8_unicode_ci NOT NULL,
-  `description` varchar(2048) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(2048) COLLATE utf8_unicode_ci,
   `personsNeeded` int(2) NOT NULL,
   `scheduledTime` timestamp NOT NULL,
   `location` varchar(2048) COLLATE utf8_unicode_ci NOT NULL,
@@ -37,3 +42,4 @@ CREATE TABLE `tasks` (
   FOREIGN KEY (`volunteerID`) REFERENCES users(`volunteerID`),
   FOREIGN KEY (`assignerID`) REFERENCES users(`volunteerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
